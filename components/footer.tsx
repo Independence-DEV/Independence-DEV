452d902267374834f822fee5f904e4d418fd7f60
i-dev <independence.dev.pro@gmail.com>
2022-07-14 21:29:23 +0300

    Logo + favicon

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,8 +8,7 @@ const Footer = () => {
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <a className="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo"/>
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Independence DEV</span>
+                <img src="/assets/logo.svg" className="mr-3 h-8" alt="Independence DEV Logo"/>
               </a>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
