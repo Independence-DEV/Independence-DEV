8b9294c630ec44c0c7bad5ebfd8b21bcf86b033c
Geof <geoffrey.bernicot@gmail.com>
2022-08-11 13:51:04 +0300

    faute orthographe + padding hero

--- components/formation.tsx
+++ components/formation.tsx
@@ -35,7 +35,7 @@ const Formation = () => {
                           <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
                       </svg>
                       <span className="ml-4 flex items-start flex-col leading-none">
-          <span className="text-xs text-gray-500 mb-1 dark:group-hover:text-white">Videos gratuites</span>
+          <span className="text-xs text-gray-500 mb-1 dark:group-hover:text-white">Vidéos gratuites</span>
           <span className="title-font font-medium dark:group-hover:text-white">YouTube</span>
         </span>
                   </button>
--- components/post-header.tsx
+++ components/post-header.tsx
@@ -31,7 +31,7 @@ const PostHeader = ({ title, coverImage, date, youtube, excerpt }: Props) => {
                                  viewBox="0 0 576 512">
                                 <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
                             </svg>
-                            <span className="text-s text-gray-400 ml-2 flex items-start flex-col leading-none dark:group-hover:text-white">Voir la video YouTube</span>
+                            <span className="text-s text-gray-400 ml-2 flex items-start flex-col leading-none dark:group-hover:text-white">Voir la vidéo YouTube</span>
                         </button>
                     </a>
                 </div>
